99b020c8c843b5d7e1d74d55de0892cd1264aae1
Banacount <uniquejmal@gmail.com>
2026-04-29 13:32:25 +0800

    Student A: added "x" and write operations.

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ FILE_NAME = "data.txt"
 
 try:
     with open("data.txt", "x") as file:
-        file.write("Hello world\n")
+        file.write("")
 except FileExistsError:
     print("Save file detected!")
